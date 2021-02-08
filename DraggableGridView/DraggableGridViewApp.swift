0602a320ccd3e5306ee8bd4e198ad7c87982c458
Heath Hwang <heath.supergene@gmail.com>
2021-02-08 12:30:04 +0900

    Initial Commit

---
+++ DraggableGridView/DraggableGridViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DraggableGridViewApp.swift
+//  DraggableGridView
+//
+//  Created by Heath Hwang on 2/8/21.
+//
+
+import SwiftUI
+
+@main
+struct DraggableGridViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
